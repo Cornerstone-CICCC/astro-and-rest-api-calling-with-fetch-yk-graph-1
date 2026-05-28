fee23ec525be0b638d7d3e9df60d0909fc8116a0
yk-graph <yokokura.tatsuya@gmail.com>
2026-05-27 21:08:47 -0700

    refactor: routes controll

--- backend/src/routes/post.router.ts
+++ backend/src/routes/post.router.ts
@@ -4,12 +4,12 @@ import { PostController } from '@/controllers/post.controller'
 export const createPostRouter = (postController: PostController): Router => {
   const router = Router()
 
-  router.get('/posts', postController.getPosts)
-  router.get('/posts/:id', postController.getPost)
-  router.post('/posts', postController.create)
-  router.put('/posts/:id', postController.replace)
-  router.patch('/posts/:id', postController.update)
-  router.delete('/posts/:id', postController.delete)
+  router.get('/', postController.getPosts)
+  router.get('/:id', postController.getPost)
+  router.post('/', postController.create)
+  router.put('/:id', postController.replace)
+  router.patch('/:id', postController.update)
+  router.delete('/:id', postController.delete)
 
   return router
 }
